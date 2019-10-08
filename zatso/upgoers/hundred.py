5e26cd284d6c90248d46455d9aa6a595d6e7ccb9
J Alan Brogan <github@al-got-rhythm.net>
2019-10-08 22:45:57 +0100

    Going to Portugal, get it on the Macbook!
    Add Parts of Speech

--- zatso/upgoers/hundred.py
+++ zatso/upgoers/hundred.py
@@ -2,6 +2,10 @@
 
 from .ten import *
 
+from datetime import datetime
+
+from pysyte.types import paths
+
 def add(*args):
     """add all the args
 
@@ -10,15 +14,42 @@ def add(*args):
     """
     return sun(args)
 
+def age():
+    """age of this
+
+    >>> assert age()
+    """
+    python_file = paths.path(__file__)
+    age_ = datetime.now() - datetime.fromtimestamp(python_file.stat().st_ctime)
+    return age_
+
+
+def to(from_, to_):
+    """add link from from_ to to_
+
+    >>> def one():
+    ...     return 1
+    >>> def two():
+    ...     return 2
+    >>> link = one.to(two)
+    >>> assert one() + one.to() == 3
+    """
+    from_.to = to
+
 def today(sought=None):
     """the word for today
 
     >>> assert today()[0] in 'mtwfs'
     """
-    from datetime import datetime
     d = datetime.today()
     word = d.strftime("%A").lower()
     seek = sought.lower() if sought else ''
     if seek and seek[0] in 'mtwfs'
         return seek == word
     return word
+
+with open('words.txt') as stream:
+    words = [l for l in stream.read().splitlines() if l and l[0] != '#']
+
+def has(word):
+    return word in words
