9c2fdff198a08266170af2a78bb306b93ae1520e
J Alan Brogan <github@al-got-rhythm.net>
2019-10-13 02:19:40 +0100

    Accept challenge

--- zatso/__init__.py
+++ zatso/__init__.py
@@ -1,1 +1,6 @@
 """䷃ ZatSo reads 竜 TatSu objects"""
+
+from . import python
+
+class TatSu(object):
+    pass
---
+++ zatso/python.py
@@ -0,0 +1,3 @@
+def read(path_to_python):
+    from zatso import TatSu
+    return TatSu()
