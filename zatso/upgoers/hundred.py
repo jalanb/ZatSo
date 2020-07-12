1f272b7380aec503aafc310347bd9ac6e78234fd
J Alan Brogan <github@al-got-rhythm.net>
2020-07-12 09:40:06 +0100

    this n that

--- zatso/upgoers/hundred.py
+++ zatso/upgoers/hundred.py
@@ -1,6 +1,10 @@
 """Provide the 100 most common words"""
 
-from .ten import *
+import re
+from datetime import datetime
+
+from pysyte.types import paths
+
 
 def add(*args):
     """add all the args
@@ -10,10 +14,24 @@ def add(*args):
     """
     return sun(args)
 
+def age(that=None):
+    """age of this or that
+
+    >>> assert age()
+    >>> assert age(add) == age(age)
+    """
+    if that:
+        raise NotImplementedError(f'age({that})')
+    path_to_this = paths.path(__file__)
+    age_ = datetime.now() - datetime.fromtimestamp(path_to_this.stat().st_ctime)
+    return age_
+
+
 def today(sought=None):
     """the word for today
 
-    >>> assert today()[0] in 'mtwfs'
+    >>> assert re.match('[MTWFS][adehinorstu]*day', today())
+    >>> assert re.match('[MTWFS][adehinorstu]*day', 'Thursday')
     """
     from datetime import datetime
     d = datetime.today()
@@ -22,3 +40,10 @@ def today(sought=None):
     if seek and seek[0] in 'mtwfs'
         return seek == word
     return word
+
+
+with open('words.txt') as stream:
+    words = [l for l in stream.read().splitlines() if l and l[0] != '#']
+
+def has(word):
+    return word in words
