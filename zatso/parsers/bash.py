33cb748b77b6c86ac032f8c3f2d1607c09c596d2
J Alan Brogan <github@al-got-rhythm.net>
2024-04-11 00:21:24 +0100

    Test bash parser

---
+++ zatso/parsers/bash.py
@@ -0,0 +1,7 @@
+from pysyte.types import paths
+
+from zatso.parsers.asts import AST
+
+def parse(p: paths.StringPath) -> AST:
+    raise NotImplementedError("zatso.parsers.bash.parse()")
+    breakpoint()
