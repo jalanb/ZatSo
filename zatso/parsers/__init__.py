c2d1de9308d08c61df334c71c572c2610e3610e1
J Alan Brogan <github@al-got-rhythm.net>
2024-04-11 10:19:54 +0100

    fake imports from tatsu into zatso

--- zatso/parsers/__init__.py
+++ zatso/parsers/__init__.py
@@ -1,7 +1,34 @@
+import importlib
+
+from pysyte.types.paths import path
+
+
+def make_parse_function(module):
+    def parse_function(filename):
+        parser = Parser()
+        p = path(filename)
+        return parser.parse(p.text(), filename=filename)
+
+    Parser = getattr(module, 'Parser')
+    return parse_function
 
 
 def on_import():
-    from ..tatsu import parsers
-    breakpoint()
+    here = path(__file__).parent
+    there = here.parent / "tatsu/parsers"
+    result = []
+    for source in there.files("*.py"):
+        if source.name == "__init__.py":
+            continue
+        module_name = source.stem_name
+        module_path = f"..tatsu.parsers.{module_name}"
+        module = importlib.import_module(module_path, package="zatso.parsers")
+        try:
+            parse_function = make_parse_function(module)
+        except AttributeError:
+            continue
+        globals()[module_name] = parse_function
+        result.append(module_name)
+    return result
 
-fred = on_import()
+__all__ = on_import()
