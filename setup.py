52e831400f345eaa38e37bf087f7a95fd07fed44
J Alan Brogan <github@al-got-rhythm.net>
2024-04-11 02:52:26 +0100

    Update setup files for more modern Python

--- setup.py
+++ setup.py
@@ -1,63 +1,11 @@
 """Set up the zatso project"""
 
-import os
 from setuptools import find_packages
 from setuptools import setup
 
-import zatso
-
-def package_files(directory):
-    paths = []
-    test_extensions = ('.test', '.tests')
-    for (path, directories, filenames) in os.walk(directory):
-        for filename in filenames:
-            extension = os.path.splitext(filename)[-1]
-            if extension not in test_extensions:
-                continue
-            paths.append(os.path.join('..', path, filename))
-    return paths
-
-extra_files = package_files('zatso')
-
-
-description = zatso.__doc__
-headline = description.splitlines()[0]
-
 setup(
-    name=zatso.__name__,
     packages=find_packages(),
-    package_data={'': package_files('zatso')},
-    version=zatso.__version__,
-    url='https://github.com/jalanb/%s' % zatso.__name__,
-    download_url='https://github.com/jalanb/%s/tarball/v%s' % (
-        zatso.__name__, zatso.__version__),
-    license='MIT License',
-    author='J Alan Brogan',
-    author_email='github@al-got-rhythm.net',
-    description=description,
-    long_description=headline,
-    platforms='any',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Environment :: Console',
-        'Development Status :: 1 - Planning',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-        'Framework :: Scrapy',
-        'Framework :: IPython',
-        'Topic :: Education',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Bug Tracking',
-        'Topic :: Software Development :: Code Generators',
-        'Topic :: Software Development :: Debuggers',
-        'Topic :: Software Development :: Interpreters',
-        'Topic :: Software Development :: Quality Assurance',
-        'Topic :: Software Development :: Testing',
-        'Topic :: Text Processing :: Linguistic',
-        'License :: OSI Approved :: MIT License',
-    ],
+    include_package_data=True,
     install_requires=[
         'pym',
         'pysyte',
@@ -66,16 +14,5 @@ setup(
         'spacy',
         'tatsu',
     ],
-#     entry_points = {
-#         'console_scripts': [
-#             'zat = zatso.cli.bin:zat'
-#         ]
-#     },
-    scripts=[
-        'bin/zat',
-    ],
     tests_require=['py.test'],
-    extras_require={
-        'testing': ['py.test'],
-    }
 )
